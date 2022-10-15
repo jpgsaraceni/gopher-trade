54e581233553fc1d649416c4200d220f79b73e83
João Saraceni <jpgomes@id.uff.br>
2022-10-15 15:28:18 -0300

    chore: add use case interface

---
+++ app/domain/exchange.go
@@ -0,0 +1,12 @@
+package domain
+
+import (
+	"context"
+
+	"github.com/jpgsaraceni/gopher-trade/app/domain/exchange"
+)
+
+//go:generate moq -fmt goimports -out exchange_mock.go . Exchange
+type Exchange interface {
+	CreateExchange(ctx context.Context, input exchange.CreateExchangeInput) (exchange.CreateExchangeOutput, error)
+}
