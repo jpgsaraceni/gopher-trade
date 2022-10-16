f42f8e9bd873fccf024d79bea941e5accb8c88f9
João Saraceni <jpgomes@id.uff.br>
2022-10-16 18:18:23 -0300

    chore: add vos convert function

--- app/domain/vos/currency_codes.go
+++ app/domain/vos/currency_codes.go
@@ -5,3 +5,12 @@ type CurrencyCode string
 func (c CurrencyCode) String() string {
 	return string(c)
 }
+
+func ListOfCodesToString(codes ...CurrencyCode) []string {
+	output := make([]string, 0, len(codes))
+	for _, code := range codes {
+		output = append(output, code.String())
+	}
+
+	return output
+}
