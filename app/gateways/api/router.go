42eb54da0e92c710963543881e4fe6d2c967e3ba
João Saraceni <jpgomes@id.uff.br>
2022-10-15 22:50:31 -0300

    feat: add swagger endpoint and cofigure cors

--- app/gateways/api/router.go
+++ app/gateways/api/router.go
@@ -4,17 +4,29 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	httpSwagger "github.com/swaggo/http-swagger"
 
 	"github.com/jpgsaraceni/gopher-trade/app/domain/exchange"
 	"github.com/jpgsaraceni/gopher-trade/app/gateways/api/handlers/exchanges"
 )
 
 func NewRouter(exchangeUC exchange.UseCase) http.Handler {
-	exchangeHandler := exchanges.NewHandler(exchangeUC)
-
+	corsOptions := cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "HEAD", "POST", "PUT", "OPTIONS"},
+		AllowedHeaders: []string{"X-Requested-With", "Origin", "Content-Type", "Authorization"},
+	}
 	r := chi.NewRouter()
+	r.Use(cors.Handler(corsOptions))
 
+	exchangeHandler := exchanges.NewHandler(exchangeUC)
 	r.Post("/exchanges", exchangeHandler.CreateExchange)
+
+	r.Get("/swagger", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "swagger/index.html", http.StatusMovedPermanently)
+	})
+	r.Get("/swagger/*", httpSwagger.WrapHandler)
 
 	return r
 }
