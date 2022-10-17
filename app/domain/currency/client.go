7a7ecf7a23b64cd1a1f5ce16ec95251c8c459c85
João Saraceni <jpgomes@id.uff.br>
2022-10-17 00:55:43 -0300

    feat: add client interface

---
+++ app/domain/currency/client.go
@@ -0,0 +1,12 @@
+package currency
+
+import (
+	"context"
+
+	"github.com/jpgsaraceni/gopher-trade/app/domain/vos"
+)
+
+//go:generate moq -fmt goimports -out client_mock.go . Client
+type Client interface {
+	GetRates(ctx context.Context) (vos.DefaultRates, error)
+}
